878290cad66839a4315a65c66a665dd38a01d8c7
samtx08 <65099561+samtx08@users.noreply.github.com>
2023-11-03 17:37:52 -0300

    Telephone number validator.js

--- JavaScript Algorithms and Data Structures/telephone-number-validator.js
+++ JavaScript Algorithms and Data Structures/telephone-number-validator.js
@@ -1,1 +1,1 @@
-const telephoneCheck = (str) => /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-])?(\d{3})([\s\-])?(\d{4})$/.test(str);
+const telephoneCheck = (str) => /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-])?(\d{3})([\s\-])?(\d{4})$/g.test(str);
